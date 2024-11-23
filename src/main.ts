83e4a3029787ae793dbd969409742150573a8e54
LucasCid <lucasfelipaaa@gmail.com>
2024-11-23 09:44:52 -0300

    feat: swagger for documentation added to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
+  app.enableCors({origin : "*"});
+  
+  const config = new DocumentBuilder()  
+  .setTitle('TablesBuilder')
+  .setDescription('API documentada para o TablesBuilder')
+  .setVersion('1.0')
+  .addTag('api-tables-builder')
+  .build()
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
